48f52809c0dc1f5eadb949184df81e35f8a77218
Admin <admin@MacBook-Air-Admin.local>
2024-01-08 23:54:30 +0500

    create-registor

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,6 +3,7 @@ import "./Register.css"
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import registor_image from "../../assets/puzzle_image_registor.png"
+import registor_logo from "../../assets/logo.png"
 
 function Register() {
   const naviagte = useNavigate()
@@ -32,30 +33,42 @@ function Register() {
     naviagte('/login')
   }
 
+  const shoot_login = ()=> {
+    naviagte('/login')
+  }
+
+  const shootHome = ()=> {
+    naviagte('/')
+  }
+
   return (
-    <div className="registor">
-      <div className="form">
-        <form onSubmit={addToHandler} action="">  
-        <h1>регистрация</h1>
-          <span>Фамилия*</span>
-          <input required type="text" name='surname' placeholder='Фамилия' />
-          <span>Имя*</span>
-          <input required type="text" name='name' placeholder='Имя' />
-          <span>Отчество*</span>
-          <input required type="text" name='mid_name' placeholder='Отчество' />
-          <span>Возраст*</span>
-          <input required type="number" name='age' placeholder='Возраст' />
-          <span>Телефон*</span>
-          <input required type="number" name='phone' placeholder='Телефон' />
-          <span>E-mail</span>
-          <input required type="email" name='email' placeholder='Введите e-mail' />
-          <span>Пароль*</span>
-          <input required type="password" name='password' placeholder='Пароль' />
-          <button className=''>Зарегистрироваться</button>
-        </form>
-      </div>
-      <div className="registor-image">
-        <img src={registor_image} alt="" />
+    <div className="registor-container">
+      <div className="registor">
+        <div className="form">
+          <form onSubmit={addToHandler} action="">
+            <img onClick={shootHome} className='registor-logo' src={registor_logo} alt="" />
+            <h1>регистрация</h1>
+            <span>Фамилия*</span>
+            <input required type="text" name='surname' placeholder='Фамилия' />
+            <span>Имя*</span>
+            <input required type="text" name='name' placeholder='Имя' />
+            <span>Отчество*</span>
+            <input required type="text" name='mid_name' placeholder='Отчество' />
+            <span>Возраст*</span>
+            <input required type="number" name='age' placeholder='Возраст' />
+            <span>Телефон*</span>
+            <input required type="number" name='phone' placeholder='Телефон' />
+            <span>E-mail</span>
+            <input required type="email" name='email' placeholder='Введите e-mail' />
+            <span>Пароль*</span>
+            <input required type="password" name='password' placeholder='Пароль' />
+            <button className='btn registor-btn'>Зарегистрироваться</button>
+          </form>
+          <button onClick={shoot_login} className='btn registor-login-btn'>Login In</button>
+        </div>
+        <div className="registor-image">
+          <img src={registor_image} alt="" />
+        </div>
       </div>
     </div>
   )
--- src/pages/Sign In/Login.jsx
+++ src/pages/Sign In/Login.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import login_logo from "../../assets/logo.png"
+import "./Login.css"
+import login_image from "../../assets/puzzle_image_registor.png"
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
+  const navigate = useNavigate()
+  const shootRegistor = ()=> {
+    navigate('/register')
+  }
+
+  const shootHome = ()=>{
+    navigate('/')
+  }
   return (
-    <div>Login</div>
+    <div className="login-container">
+      <div className="login">
+        <div className="form">
+          <form  action="">
+            <img onClick={shootHome} className='login-logo' src={login_logo} alt="" />
+            <h1>Вход</h1>
+            <span>E-mail</span>
+            <input required type="email" name='email' placeholder='Введите e-mail' />
+            <span>Пароль*</span>
+            <input required type="password" name='password' placeholder='Пароль' />
+            <button className='btn login-btn'>Вход</button>
+          </form>
+          <button onClick={shootRegistor}  className='btn login-login-btn'>Зарегистрироваться</button>
+        </div>
+        <div className="login-image">
+          <img src={login_image} alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
